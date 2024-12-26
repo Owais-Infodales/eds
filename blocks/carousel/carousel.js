4e60950e2e44e31b0e704bb8f569b5751a0d4e68
Owais-Infodales <128490173+Owais-Infodales@users.noreply.github.com>
2024-12-26 18:09:12 +0530

    Update carousel.js

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,20 +1,19 @@
 import { fetchPlaceholders, getMetadata } from '../../scripts/aem.js';
+
 const placeholders = await fetchPlaceholders(getMetadata('locale'));
-
 const { btnNxt, btnPre } = placeholders;
-export default function decorate(block) {
 
-  console.log('placeholders ---> ', placeholders, btnNxt, btnPre);
+export default function decorate(block) {
   const rows = [...block.children];
   [...block.children].forEach((row, r) => {
-    if (r == 0) {
+    if (r === 0) {
       const nextbtn = document.createElement('button');
       nextbtn.classList.add('btn');
       nextbtn.classList.add('btn-next');
       const node = document.createTextNode(btnNxt);
       nextbtn.append(node);
       row.replaceWith(nextbtn);
-    } else if (r == rows.length - 1) {
+    } else if (r === rows.length - 1) {
       const prebtn = document.createElement('button');
       prebtn.classList.add('btn');
       prebtn.classList.add('btn-prev');
@@ -24,7 +23,7 @@ export default function decorate(block) {
     } else {
       row.classList.add('slide');
       [...row.children].forEach((col, c) => {
-        if (c == 1) {
+        if (c === 1) {
           col.classList.add('slide-text');
         }
       });
@@ -33,47 +32,40 @@ export default function decorate(block) {
 
   const slides = document.querySelectorAll('.slide');
 
-  // loop through slides and set each slides translateX
+  // Loop through slides and set each slide's translateX
   slides.forEach((slide, indx) => {
     slide.style.transform = `translatex(${indx * 100}%)`;
   });
 
-  // select next slide button
+  // Select next slide button
   const nextSlide = document.querySelector('.btn-next');
 
-  // current slide counter
+  // Current slide counter
   let curSlide = 0;
-  // maximum number of slides
-  let maxSlide = slides.length - 1;
 
-  // add event listener and navigation functionality
-  nextSlide.addEventListener('click', function () {
-    // check if current slide is the last and reset current slide
-    if (curSlide === maxSlide) {
-      curSlide = 0;
-    } else {
-      curSlide++;
-    }
+  // Maximum number of slides
+  const maxSlide = slides.length - 1;
 
-    //   move slide by -100%
+  // Add event listener and navigation functionality
+  nextSlide.addEventListener('click', () => {
+    // Check if current slide is the last and reset current slide
+    curSlide = curSlide === maxSlide ? 0 : curSlide + 1;
+
+    // Move slide by -100%
     slides.forEach((slide, indx) => {
       slide.style.transform = `translatex(${100 * (indx - curSlide)}%)`;
     });
   });
 
-  // select next slide button
+  // Select previous slide button
   const prevSlide = document.querySelector('.btn-prev');
 
-  // add event listener and navigation functionality
-  prevSlide.addEventListener('click', function () {
-    // check if current slide is the first and reset current slide to last
-    if (curSlide === 0) {
-      curSlide = maxSlide;
-    } else {
-      curSlide--;
-    }
+  // Add event listener and navigation functionality
+  prevSlide.addEventListener('click', () => {
+    // Check if current slide is the first and reset current slide to last
+    curSlide = curSlide === 0 ? maxSlide : curSlide - 1;
 
-    //   move slide by 100%
+    // Move slide by 100%
     slides.forEach((slide, indx) => {
       slide.style.transform = `translatex(${100 * (indx - curSlide)}%)`;
     });

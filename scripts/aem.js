ddef7ea99f2c5a004459755839793ada0d8af712
Owais-Infodales <128490173+Owais-Infodales@users.noreply.github.com>
2024-12-31 15:51:30 +0530

    Update aem.js

--- scripts/aem.js
+++ scripts/aem.js
@@ -506,14 +506,14 @@ async function fetchPlaceholders(prefix = 'default') {
   window.placeholders = window.placeholders || {};
   if (!window.placeholders[prefix]) {
     window.placeholders[prefix] = new Promise((resolve) => {
-            let localizeUrl = new URL(
-              `${window.location.origin}/${prefix}/placeholder.json`
+    let localizeUrl = new URL(
+            `${window.location.origin}/${prefix}/placeholder.json`
+    );
+    if (prefix === "") {
+            localizeUrl = new URL(
+                    `${window.location.origin}placeholder.json`
             );
-            if (prefix === "") {
-              localizeUrl = new URL(
-                `${window.location.origin}placeholder.json`
-              );
-            }
+    }
       fetch(localizeUrl)
         .then((resp) => {
           if (resp.ok) {

efa75c651a173a843031293623fe7e2f21de7cd5
Owais-Infodales <128490173+Owais-Infodales@users.noreply.github.com>
2024-12-31 14:36:49 +0530

    Update author.js

--- blocks/author/author.js
+++ blocks/author/author.js
@@ -1,47 +1,61 @@
-import { fetchPlaceholders, getMetadata } from "../../scripts/aem.js";
-const placeholders = await fetchPlaceholders(getMetadata("locale"));
+import { fetchPlaceholders, getMetadata } from '../../scripts/aem.js';
+
+const placeholders = await fetchPlaceholders(getMetadata('locale'));
 console.log(placeholders);
-const { authorDetails, firstName, lastName, occupation, bio, topics } =
-  placeholders;
+
+const {
+  authorDetails,
+  firstName,
+  lastName,
+  occupation,
+  bio,
+  topics,
+} = placeholders;
 
 export default function decorate(block) {
-  const headingDiv = document.createElement("div");
-  headingDiv.classList.add("table-heading");
+  const headingDiv = document.createElement('div');
+  headingDiv.classList.add('table-heading');
   const htext = document.createTextNode(authorDetails);
-  const headingH1 = document.createElement("h1");
+  const headingH1 = document.createElement('h1');
   headingH1.append(htext);
   headingDiv.append(headingH1);
 
-  const table = document.createElement("table");
-  let tr = document.createElement("tr");
-  //let ad=document.createElement("th");ad.appendChild(document.createTextNode(authorDetails));tr.append(ad);
-  let fn = document.createElement("th");
+  const table = document.createElement('table');
+  const tr = document.createElement('tr');
+
+  const fn = document.createElement('th');
   fn.appendChild(document.createTextNode(firstName));
   tr.append(fn);
-  let ln = document.createElement("th");
+
+  const ln = document.createElement('th');
   ln.appendChild(document.createTextNode(lastName));
   tr.append(ln);
-  let oc = document.createElement("th");
+
+  const oc = document.createElement('th');
   oc.appendChild(document.createTextNode(occupation));
   tr.append(oc);
-  let bi = document.createElement("th");
+
+  const bi = document.createElement('th');
   bi.appendChild(document.createTextNode(bio));
   tr.append(bi);
-  let to = document.createElement("th");
+
+  const to = document.createElement('th');
   to.appendChild(document.createTextNode(topics));
   tr.append(to);
+
   table.append(tr);
+
   [...block.children].forEach((row, r) => {
-    let trow = document.createElement("tr");
+    const trow = document.createElement('tr');
     [...row.children].forEach((col, c) => {
-      console.log(" Row : Col  ", r, c);
-      let tcol = document.createElement("td");
+      console.log('Row : Col', r, c);
+      const tcol = document.createElement('td');
       tcol.appendChild(col);
       trow.append(tcol);
-      row.replaceWith(trow);
     });
     table.append(trow);
   });
+
   block.append(headingDiv);
   block.append(table);
 }

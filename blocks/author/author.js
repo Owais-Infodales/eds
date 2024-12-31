db8c4f4cd4a327adf5e4414cac280ab722a00cb1
Owais-Infodales <128490173+Owais-Infodales@users.noreply.github.com>
2024-12-31 15:31:42 +0530

    Update author.js

--- blocks/author/author.js
+++ blocks/author/author.js
@@ -1,42 +1,30 @@
-import { fetchPlaceholders, getMetadata } from "../../scripts/aem.js";
+import { fetchPlaceholders, getMetadata } from '../../scripts/aem.js';
 
 async function createtableWithplaceholder(table) {
-  const locale = getMetadata("locale");
+  const locale = getMetadata('locale');
   const placeholders = await fetchPlaceholders(locale);
-  const {
-    fnamekey,
-    lnameKey,
-    lastName,
-    roleKey,
-    orgKey,
-    cntryKey,
-    header,
-    firstName,
-    lastname,
-    role,
-    organization,
-    country,
-  } = placeholders;
+  const { fnamekey, header, firstName } = placeholders;
 
-  let authorRow = document.createElement("tr");
-  let authorCol = document.createElement("th");
+  const authorRow = document.createElement('tr');
+  const authorCol = document.createElement('th');
   authorCol.appendChild(document.createTextNode(header));
   authorCol.colSpan = 2;
   authorRow.append(authorCol);
 
-  let firstnameRow = document.createElement("tr");
-  let firstNameCol1 = document.createElement("td");
+  const firstnameRow = document.createElement('tr');
+  const firstNameCol1 = document.createElement('td');
   firstNameCol1.appendChild(document.createTextNode(fnamekey));
-  let firstNameCol2 = document.createElement("td");
+  const firstNameCol2 = document.createElement('td');
   firstNameCol2.appendChild(document.createTextNode(firstName));
   firstnameRow.append(firstNameCol1);
   firstnameRow.append(firstNameCol2);
+
   table.append(authorRow);
   table.append(firstnameRow);
 }
 
 export default function decorate(block) {
-  const table = document.createElement("table");
+  const table = document.createElement('table');
   createtableWithplaceholder(table);
   block.replaceWith(table);
 }
